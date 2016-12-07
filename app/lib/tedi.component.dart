e4173b36d67ce9238cf259f994b55ff8542156e3
Tiennou <tiennou1962@gmail.com>
2016-12-07 15:49:29 +0100

    Add svg example component

--- app/lib/tedi.component.dart
+++ app/lib/tedi.component.dart
@@ -1,9 +1,12 @@
 import 'package:angular2/core.dart';
 
+import 'package:tedi/example/example.component.dart';
+
 @Component(
   selector:"tedi",
   templateUrl:"tedi.component.html",
-  styleUrls: const ["tedi.component.css"]
+  styleUrls: const ["tedi.component.css"],
+  directives: const [ExampleComponent]
 )
 class TediComponent {
   String hello = "Hello world !";
